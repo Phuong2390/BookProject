2175112e7848ccdcd305261b8dd714cd122c2f9e
Phuong2390 <vanphuongk38cntt@gmail.com>
2018-03-08 11:54:23 +0700

    note in .env.example

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,16 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
   development: {
       client : 'mysql',
       connection : {
-          host : 'localhost',
-          user : 'root',
-          password : ' ',
-          database : 'bookdemo'
-      }
+		  host: process.env.DB_HOST,
+		  user: process.env.DB_USER,
+		  password: process.env.DB_PASS,
+		  database: process.env.DB_DATA
+	  }
   },
 
   staging: {
